aeef8b4c0ed28b67c0103f7e3c05c1ff0aff2aa9
Saran Tejas Kaja <48532068+kajasaran@users.noreply.github.com>
2020-12-03 14:29:29 -0500

    Update string_evaluator.py

--- src/main/string_evaluator.py
+++ src/main/string_evaluator.py
@@ -4,16 +4,16 @@ class StringManipulator(object):
         return get_hello_world  # TODO - Implement solution
 
     def concatenate(self, value_to_be_added_to, value_to_add):
-        return value_to_be_added_to + value_to_add # TODO - Implement solution
+        return "value_to_be_added_to" + "value_to_add" # TODO - Implement solution
 
     def substring_inclusive(self, string_to_fetch_from, starting_index, ending_index):
-        return None # TODO - Implement solution
+        return string_to_fetch_from[starting_index:ending_index] # TODO - Implement solution
 
     def substring_exclusive(self, string_to_fetch_from, starting_index, ending_index):
         return None # TODO - Implement solution
 
     def compare(self, first_value, second_value):
-        return first_value==second_value # TODO - Implement solution
+        return "first_value"=="second_value" # TODO - Implement solution
 
     def get_middle_character(self, string_to_fetch_from):
         return None # TODO - Implement solution
