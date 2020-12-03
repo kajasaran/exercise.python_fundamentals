fb157d4641a4f8c6bf6837ba841f508186b489ea
Saran Tejas Kaja <48532068+kajasaran@users.noreply.github.com>
2020-12-03 13:56:59 -0500

    Update predicator.py

--- src/main/predicator.py
+++ src/main/predicator.py
@@ -1,14 +1,14 @@
 # Created by Leon Hunter at 3:56 PM 10/23/2020
 class Predicator(object):
     def is_greater_than_5(self, some_value):
-        some_value=6
-        return some_value # TODO - Implement solution
+        
+        return some_value>5 # TODO - Implement solution
 
     def is_greater_than_8(self, some_value):
-        return some_value # TODO - Implement solution
+        return some_value>8 # TODO - Implement solution
 
     def is_less_than_4(self, some_value):
-        return some_value # TODO - Implement solution
+        return some_value<4 # TODO - Implement solution
 
     def is_less_than_1(self, some_value):
-        return some_value # TODO - Implement solution
+        return some_value<1 # TODO - Implement solution
