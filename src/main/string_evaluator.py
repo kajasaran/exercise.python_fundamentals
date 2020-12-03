600e6eb30267894b87cf2894cb576fba452d31a6
Saran Tejas Kaja <48532068+kajasaran@users.noreply.github.com>
2020-12-03 14:37:01 -0500

    Update string_evaluator.py

--- src/main/string_evaluator.py
+++ src/main/string_evaluator.py
@@ -4,25 +4,31 @@ class StringManipulator(object):
         return get_hello_world  # TODO - Implement solution
 
     def concatenate(self, value_to_be_added_to, value_to_add):
-        return "value_to_be_added_to" + "value_to_add" # TODO - Implement solution
+        a=(value_to_be_added_to + value_to_add)
+        return a  # TODO - Implement solution
 
     def substring_inclusive(self, string_to_fetch_from, starting_index, ending_index):
-        return string_to_fetch_from[starting_index:ending_index] # TODO - Implement solution
+        f=string_to_fetch_from[starting_index:ending_index]
+        return f  # TODO - Implement solution
 
     def substring_exclusive(self, string_to_fetch_from, starting_index, ending_index):
         return None # TODO - Implement solution
 
     def compare(self, first_value, second_value):
-        return "first_value"=="second_value" # TODO - Implement solution
+        b=first_value==second_value
+        return b  # TODO - Implement solution
 
     def get_middle_character(self, string_to_fetch_from):
+        c=len(string_to_fetch_from)//2
         return None # TODO - Implement solution
 
     def get_first_word(self, string_to_fetch_from):
-        return string_to_fetch_from.split()[0] # TODO - Implement solution
+        d=string_to_fetch_from.split()[0]
+        return d # TODO - Implement solution
 
     def get_second_word(self, string_to_fetch_from):
-        return string_to_fetch_from.split()[1] # TODO - Implement solution
+        e=string_to_fetch_from.split()[1]
+        return e # TODO - Implement solution
 
     def reverse(self, string_to_be_reversed):
         return string_to_be_reversed[::-1] # TODO - Implement solution
